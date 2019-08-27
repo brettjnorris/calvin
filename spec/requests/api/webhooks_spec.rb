7d67e02fd13391cb9ef7e744c21ac0b1d89240bf
Brett J Norris <brettjnorris@gmail.com>
2019-08-26 21:46:27 -0400

    Update spec

--- spec/requests/api/webhooks_spec.rb
+++ spec/requests/api/webhooks_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Api::Webhooks", type: :request do
   describe "POST /api/webhooks" do
-    let(:request_body) { file_fixture("webhook_request.json").read }
-    let(:headers) do
-      {
-        "Content-Type" => "application/json"
-      }
-    end
+    context "with a valid request body" do
+      let(:request_body) { file_fixture("webhook_request.json").read }
+      let(:headers) do
+        {
+          "Content-Type" => "application/json"
+        }
+      end
 
-    it "returns a successful response" do
-      post "/api/webhooks", params: JSON.parse(request_body)
-      expect(response.status).to eq(200)
-    end
-    
-    it "creates a new message record" do
-      expect {
+      it "returns a successful response" do
         post "/api/webhooks", params: JSON.parse(request_body)
-      }.to change { Message.count }.by(1)
+        expect(response.status).to eq(200)
+      end
+      
+      it "creates a new message record" do
+        expect {
+          post "/api/webhooks", params: JSON.parse(request_body)
+        }.to change { Message.count }.by(1)
 
-      new_message = Message.last
+        new_message = Message.last
 
-      expect(new_message.event_type).to eq("invitee.created")
+        expect(new_message.event_type).to eq("invitee.created")
+      end
     end
   end
 end
