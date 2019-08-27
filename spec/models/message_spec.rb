16fc1e73b8b77221b256f4d992d5be804d1e0dae
Brett J Norris <brettjnorris@gmail.com>
2019-08-26 22:53:58 -0400

    Add tests for Message#update_event

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   let(:subject) { create :message }
 
+  before :each do
+    Message.skip_callback(:create, :after, :update_event)
+  end
+
+  after :each do
+    Message.set_callback(:create, :after, :update_event)
+  end
+
   describe "#event_type_s" do
     it "returns the event type string" do
       expect(subject.event_type_s).to eq("Invitee created")
@@ -21,6 +29,22 @@ RSpec.describe Message, type: :model do
       invitee = subject.invitee
       expect(invitee["uuid"]).to eq("CELGSHGWFFNK5G3T")
       expect(invitee["name"]).to eq("test")
+    end
+  end
+  
+  describe "#update_event" do
+    context "with a valid event and invitee" do
+      it "creates a new event record" do
+        expect {
+          subject.send(:update_event)
+        }.to change { Event.count }.by(1)
+      end
+
+      it "creates a new invitee record" do
+        expect {
+          subject.send(:update_event)
+        }.to change { Invitee.count }.by(1)
+      end
     end
   end
 end
